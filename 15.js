aabd3f0bf0808d7474f461d1c02788ac952ddea7
hyodong2 <167263781+hyodong2@users.noreply.github.com>
2024-05-22 09:06:43 +0900

    나머지가 1이 되는 수를 구하는 알고리즘 코드카타

---
+++ 15.js
@@ -0,0 +1,13 @@
+function solution(numbers) {
+    var answer = 0
+    
+    for(x in 2..n-1) {
+        if(n % x == 1) {
+            answer = x
+            break
+           
+        }
+    }
+    
+    return answer
+}
