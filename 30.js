4f64ea0421d1e2523ed857eda401d47669c0aa4a
hyodong2 <167263781+hyodong2@users.noreply.github.com>
2024-06-12 09:05:10 +0900

    부족한 금액 계산하기

---
+++ 30.js
@@ -0,0 +1,9 @@
+function solution(price, money, count) {
+    let totalPrice = 0;
+
+    for (let i = 1; i <= count; i++) {
+        totalPrice += price * i;
+    }
+
+    return Math.max(totalPrice - money, 0);
+}
